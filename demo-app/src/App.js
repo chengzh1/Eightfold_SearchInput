cd192a4e75334e52c35671b1fbd7d048fe70268f
Cheng Zhang <czhang@xcalar.com>
2020-11-20 12:50:07 -0800

    Initial react setup

---
+++ demo-app/src/App.js
@@ -0,0 +1,12 @@
+import SearchInput from './components/SearchInput';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ demo-app/src/components/SearchInput/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const propTypes = {};
+
+const SearchInput = () => {
+  return (
+    <div>test</div>
+  )
+};
+SearchInput.propTypes = propTypes;
+
+export default SearchInput;
